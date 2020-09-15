2de2a35d36c2173f731cb5c3713547fd31e52a6d
toannn-2309 <nguyen.ngoc.toan@sun-asterisk.com>
2020-09-15 16:27:21 +0700

    [29229] Add home page, boostrap4, jquery

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module CashFlowSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
   end
 end
