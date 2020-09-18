7c463d7cd20a38c6f9e077a95be5915614fc6038
toannn-2309 <nguyen.ngoc.toan@sun-asterisk.com>
2020-09-18 10:22:24 +0700

    [29620] Remember user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   USERS_PARAMS = %i(name email password password_confirmation group_id).freeze
   VALID_EMAIL_REGEX = Settings.validate.email.regex
 
+  attr_accessor :remember_token
+
   belongs_to :group
   has_many :requests, dependent: :destroy
   has_many :incomes, dependent: :destroy
@@ -22,13 +24,34 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost: cost
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 
   private
